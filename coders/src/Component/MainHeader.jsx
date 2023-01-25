18c4606cb4f0584d006c85ea3c0a3fc966f9e7c1
Matteo Rania <matteo.rania2014@gmail.com>
2023-01-25 16:30:43 +0100

    add some layout

--- coders/src/Component/MainHeader.jsx
+++ coders/src/Component/MainHeader.jsx
@@ -2,12 +2,12 @@ export function MainHeader() {
     return (
         <header className="main-header">
             <div className="start-wrapper">
-                <div className="start text-center flex flex-col justify-around rounded-3xl">
+                <div className="start text-center flex flex-col justify-around rounded-3xl drop-shadow-lg">
                     <div>
                         <h1 className="text-7xl text-black">CODERS</h1>
                     </div>
                     <div>
-                        <button className= "text-6xl p-2 bg-green-500 hover:bg-green-600 ">Start</button>
+                        <button className= "text-6xl p-7 ">Start</button>
                     </div>
                 </div>
             </div>
