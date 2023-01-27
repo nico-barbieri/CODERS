7cef86a0af3828024ab2634c93f0053d0feac015
Matteo Rania <matteo.rania2014@gmail.com>
2023-01-27 10:33:31 +0100

    logo

--- coders/src/Component/MainHeader.jsx
+++ coders/src/Component/MainHeader.jsx
@@ -1,10 +1,12 @@
+import logo from '../images/CODERS_extended_logo.svg'
+
 export function MainHeader() {
     return (
         <header className="main-header">
             <div className="start-wrapper">
                 <div className="start text-center flex flex-col justify-around rounded-3xl drop-shadow-lg">
-                    <div>
-                        <h1 className="text-7xl text-black">CODERS</h1>
+                    <div className='logo-container'>
+                        <img src={logo} alt="Logo" />
                     </div>
                     <div>
                         <button className= "text-6xl p-7 ">Start</button>
