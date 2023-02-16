966ef60626e14dccd5f7a7c2e4108b10b18e923e
Nico Barbieri <nicobarbieri.m@gmail.com>
2023-02-16 23:41:17 +0100

    update layout

--- game-assets/CODERS - office floor altcolor.tsx
+++ game-assets/CODERS - office floor altcolor.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="CODERS - office floor altcolor" tilewidth="16" tileheight="16" tilecount="272" columns="16">
+<tileset version="1.9" tiledversion="1.9.2" name="CODERS - office floor altcolor" tilewidth="16" tileheight="16" tilecount="304" columns="16">
  <image source="Modern_Office_Revamped_v1.1/Modern_Office_Revamped/1_Room_Builder_Office/Room_Builder_Office_16x16 altcolor.png" width="256" height="272"/>
 </tileset>
